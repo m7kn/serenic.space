49b769cbdd07a324e75ec39f200a9ce9eb354e78
Norbert Molnar <m7kn1872@gmail.com>
2022-08-31 20:18:15 +0200

    get debug value from env

--- serenic_space_project/settings.py
+++ serenic_space_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d@!w7zgsv6&vgd3(7q%idvkt53qs*ooa2zh_k$#2rz%v$%9tdt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = ['*']
 
